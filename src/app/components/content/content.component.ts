4388336b1a648a6013e7eb76963f80ce7c7ea192
Steewar Martinez <steewarmartinez@gmail.com>
2025-02-01 21:29:30 -0300

    actualizacion

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -4,8 +4,15 @@ import { Component } from '@angular/core';
   selector: 'app-content',
   imports: [],
   templateUrl: './content.component.html',
-  styleUrl: './content.component.css'
+  styleUrl: './content.component.css',
 })
 export class ContentComponent {
-
+  links = [
+    'https://img.icons8.com/color/48/000000/html-5.png',
+    'https://img.icons8.com/color/48/000000/css3.png',
+    'https://img.icons8.com/color/48/000000/javascript.png',
+    'https://img.icons8.com/color/48/000000/angularjs.png',
+    'https://img.icons8.com/color/48/000000/github--v1.png',
+    'https://img.icons8.com/color/48/000000/python.png',
+  ];
 }
